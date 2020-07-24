624e5e75632285d04b7fb69644bc395c6eac7e80
tki <tki.wroc@gmail.com>
2020-07-24 14:20:06 +0200

    Add safety check for empty pair reserves

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -218,8 +218,16 @@ export function handleSync(event: Sync): void {
 
   pair.reserve0 = convertTokenToDecimal(event.params.reserve0, token0.decimals)
   pair.reserve1 = convertTokenToDecimal(event.params.reserve1, token1.decimals)
-  pair.token0Price = pair.reserve0.div(pair.reserve1)
-  pair.token1Price = pair.reserve1.div(pair.reserve0)
+
+  if (pair.reserve1.notEqual(ZERO_BD))
+    pair.token0Price = pair.reserve0.div(pair.reserve1)
+  else
+    pair.token0Price = ZERO_BD
+  if (pair.reserve0.notEqual(ZERO_BD))
+    pair.token1Price = pair.reserve1.div(pair.reserve0)
+  else
+    pair.token1Price = ZERO_BD
+
   pair.save()
 
   // update ETH price now that reserves could have changed
