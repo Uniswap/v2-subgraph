f037a3cece235326fb55eb3924435ef2db24f85b
Robert Seitz <robert.seitz@uniswap.org>
2025-09-23 09:42:41 -0400

    add virtual to the base whitelist

--- config/base/chain.ts
+++ config/base/chain.ts
@@ -10,6 +10,7 @@ export const WHITELIST: string[] = [
   '0x4200000000000000000000000000000000000006', // WETH
   '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913', // USDC
   '0xd9aaec86b65d86f6a7b5b1b0c42ffa531710b6ca', // USDbCC
+  '0x0b3e328455c4059eeb9e3f84b5543f74e24e7e1b', // VIRTUAL
 ]
 
 export const STABLECOINS = [
