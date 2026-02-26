765ee34292e00b3016dd89960661398047888945
Crishna Iyengar <ciyengar3@gmail.com>
2026-02-26 11:37:51 -0500

    config

--- config/tempo-mainnet/chain.ts
+++ config/tempo-mainnet/chain.ts
@@ -5,11 +5,11 @@ export const FACTORY_ADDRESS = '0xf9ec577a4e45b5278bb7cf60fcbc20c3acaef68f'
 const PATHUSD = '0x20C0000000000000000000000000000000000000'.toLowerCase()
 const USDT0 = '0x20c00000000000000000000014f22ca97301eb73'.toLowerCase()
 const USDCE = '0x20C000000000000000000000b9537d11c60E8b50'.toLowerCase()
-const PATH_USD_USDC= "0xa9c9f6912ef077000356340421b5c9d1d094123e".toLowerCase()
 const EURC = '0x20c0000000000000000000001621e21F71CF12fb'.toLowerCase()
 
+const PATHUSD_USDC_PAIR = "0xa9c9f6912ef077000356340421b5c9d1d094123e".toLowerCase()
 export const REFERENCE_TOKEN = PATHUSD
-export const STABLE_TOKEN_PAIRS = [PATH_USD_USDC]
+export const STABLE_TOKEN_PAIRS = [PATHUSD_USDC_PAIR]
 
 // token where amounts should contribute to tracked volume and liquidity
 export const WHITELIST: string[] = [PATHUSD, USDT0, USDCE, EURC]
